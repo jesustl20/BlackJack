e6a385809ec9dfafb2e68cc82fb0ff84f5958bc4
jesustl20 <jesustercero01@gmail.com>
2022-10-19 17:58:36 +0200

    Update Vista.java
    Refactorizar variables de los jlabels

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Color;
+import javax.swing.JButton;
 
 public class Vista extends JFrame {
 
@@ -40,124 +41,132 @@ public class Vista extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel l = new JLabel("");
-		l.setForeground(Color.BLACK);
-		l.setBackground(Color.WHITE);
-		l.setBounds(51, 54, 127, 180);
-		contentPane.add(l);
+		JLabel lbl1 = new JLabel("");
+		lbl1.setForeground(Color.BLACK);
+		lbl1.setBackground(Color.WHITE);
+		lbl1.setBounds(51, 54, 127, 180);
+		contentPane.add(lbl1);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setBounds(187, 54, 127, 180);
-		contentPane.add(lblNewLabel_1);
+		JLabel lbl2 = new JLabel("");
+		lbl2.setBounds(187, 54, 127, 180);
+		contentPane.add(lbl2);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setBounds(324, 54, 127, 180);
-		contentPane.add(lblNewLabel_2);
+		JLabel lbl3 = new JLabel("");
+		lbl3.setBounds(324, 54, 127, 180);
+		contentPane.add(lbl3);
 		
-		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setBounds(461, 54, 127, 180);
-		contentPane.add(lblNewLabel_3);
+		JLabel lbl4 = new JLabel("");
+		lbl4.setBounds(461, 54, 127, 180);
+		contentPane.add(lbl4);
 		
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setForeground(Color.BLACK);
-		lblNewLabel_4.setBackground(Color.WHITE);
-		lblNewLabel_4.setBounds(598, 54, 127, 180);
-		contentPane.add(lblNewLabel_4);
+		JLabel lbl5 = new JLabel("");
+		lbl5.setForeground(Color.BLACK);
+		lbl5.setBackground(Color.WHITE);
+		lbl5.setBounds(598, 54, 127, 180);
+		contentPane.add(lbl5);
 		
-		JLabel lblNewLabel_5 = new JLabel("");
-		lblNewLabel_5.setForeground(Color.BLACK);
-		lblNewLabel_5.setBackground(Color.WHITE);
-		lblNewLabel_5.setBounds(735, 54, 127, 180);
-		contentPane.add(lblNewLabel_5);
+		JLabel lbl6 = new JLabel("");
+		lbl6.setForeground(Color.BLACK);
+		lbl6.setBackground(Color.WHITE);
+		lbl6.setBounds(735, 54, 127, 180);
+		contentPane.add(lbl6);
 		
-		JLabel lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setForeground(Color.BLACK);
-		lblNewLabel_6.setBackground(Color.WHITE);
-		lblNewLabel_6.setBounds(51, 245, 127, 180);
-		contentPane.add(lblNewLabel_6);
+		JLabel lbl7 = new JLabel("");
+		lbl7.setForeground(Color.BLACK);
+		lbl7.setBackground(Color.WHITE);
+		lbl7.setBounds(51, 245, 127, 180);
+		contentPane.add(lbl7);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("");
-		lblNewLabel_1_1.setBounds(187, 245, 127, 180);
-		contentPane.add(lblNewLabel_1_1);
+		JLabel lbl8 = new JLabel("");
+		lbl8.setBounds(187, 245, 127, 180);
+		contentPane.add(lbl8);
 		
-		JLabel lblNewLabel_2_1 = new JLabel("");
-		lblNewLabel_2_1.setBounds(324, 245, 127, 180);
-		contentPane.add(lblNewLabel_2_1);
+		JLabel lbl9 = new JLabel("");
+		lbl9.setBounds(324, 245, 127, 180);
+		contentPane.add(lbl9);
 		
-		JLabel lblNewLabel_3_1 = new JLabel("");
-		lblNewLabel_3_1.setBounds(461, 245, 127, 180);
-		contentPane.add(lblNewLabel_3_1);
+		JLabel lbl10 = new JLabel("");
+		lbl10.setBounds(461, 245, 127, 180);
+		contentPane.add(lbl10);
 		
-		JLabel lblNewLabel_4_1 = new JLabel("");
-		lblNewLabel_4_1.setForeground(Color.BLACK);
-		lblNewLabel_4_1.setBackground(Color.WHITE);
-		lblNewLabel_4_1.setBounds(598, 245, 127, 180);
-		contentPane.add(lblNewLabel_4_1);
+		JLabel lbl11 = new JLabel("");
+		lbl11.setForeground(Color.BLACK);
+		lbl11.setBackground(Color.WHITE);
+		lbl11.setBounds(598, 245, 127, 180);
+		contentPane.add(lbl11);
 		
-		JLabel lblNewLabel_5_1 = new JLabel("");
-		lblNewLabel_5_1.setForeground(Color.BLACK);
-		lblNewLabel_5_1.setBackground(Color.WHITE);
-		lblNewLabel_5_1.setBounds(735, 245, 127, 180);
-		contentPane.add(lblNewLabel_5_1);
+		JLabel lbl12 = new JLabel("");
+		lbl12.setForeground(Color.BLACK);
+		lbl12.setBackground(Color.WHITE);
+		lbl12.setBounds(735, 245, 127, 180);
+		contentPane.add(lbl12);
 		
-		JLabel l_1 = new JLabel("");
-		l_1.setForeground(Color.BLACK);
-		l_1.setBackground(Color.WHITE);
-		l_1.setBounds(51, 477, 127, 180);
-		contentPane.add(l_1);
+		JLabel lbl13 = new JLabel("");
+		lbl13.setForeground(Color.BLACK);
+		lbl13.setBackground(Color.WHITE);
+		lbl13.setBounds(51, 477, 127, 180);
+		contentPane.add(lbl13);
 		
-		JLabel lblNewLabel_1_2 = new JLabel("");
-		lblNewLabel_1_2.setBounds(187, 477, 127, 180);
-		contentPane.add(lblNewLabel_1_2);
+		JLabel lbl14 = new JLabel("");
+		lbl14.setBounds(187, 477, 127, 180);
+		contentPane.add(lbl14);
 		
-		JLabel lblNewLabel_2_2 = new JLabel("");
-		lblNewLabel_2_2.setBounds(324, 477, 127, 180);
-		contentPane.add(lblNewLabel_2_2);
+		JLabel lbl15 = new JLabel("");
+		lbl15.setBounds(324, 477, 127, 180);
+		contentPane.add(lbl15);
 		
-		JLabel lblNewLabel_3_2 = new JLabel("");
-		lblNewLabel_3_2.setBounds(461, 477, 127, 180);
-		contentPane.add(lblNewLabel_3_2);
+		JLabel lbl16 = new JLabel("");
+		lbl16.setBounds(461, 477, 127, 180);
+		contentPane.add(lbl16);
 		
-		JLabel lblNewLabel_4_2 = new JLabel("");
-		lblNewLabel_4_2.setForeground(Color.BLACK);
-		lblNewLabel_4_2.setBackground(Color.WHITE);
-		lblNewLabel_4_2.setBounds(598, 477, 127, 180);
-		contentPane.add(lblNewLabel_4_2);
+		JLabel lbl17 = new JLabel("");
+		lbl17.setForeground(Color.BLACK);
+		lbl17.setBackground(Color.WHITE);
+		lbl17.setBounds(598, 477, 127, 180);
+		contentPane.add(lbl17);
 		
-		JLabel lblNewLabel_5_2 = new JLabel("");
-		lblNewLabel_5_2.setForeground(Color.BLACK);
-		lblNewLabel_5_2.setBackground(Color.WHITE);
-		lblNewLabel_5_2.setBounds(735, 477, 127, 180);
-		contentPane.add(lblNewLabel_5_2);
+		JLabel lbl18 = new JLabel("");
+		lbl18.setForeground(Color.BLACK);
+		lbl18.setBackground(Color.WHITE);
+		lbl18.setBounds(735, 477, 127, 180);
+		contentPane.add(lbl18);
 		
-		JLabel lblNewLabel_6_1 = new JLabel("");
-		lblNewLabel_6_1.setForeground(Color.BLACK);
-		lblNewLabel_6_1.setBackground(Color.WHITE);
-		lblNewLabel_6_1.setBounds(51, 668, 127, 180);
-		contentPane.add(lblNewLabel_6_1);
+		JLabel lbl19 = new JLabel("");
+		lbl19.setForeground(Color.BLACK);
+		lbl19.setBackground(Color.WHITE);
+		lbl19.setBounds(51, 668, 127, 180);
+		contentPane.add(lbl19);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("");
-		lblNewLabel_1_1_1.setBounds(187, 668, 127, 180);
-		contentPane.add(lblNewLabel_1_1_1);
+		JLabel lbl20 = new JLabel("");
+		lbl20.setBounds(187, 668, 127, 180);
+		contentPane.add(lbl20);
 		
-		JLabel lblNewLabel_2_1_1 = new JLabel("");
-		lblNewLabel_2_1_1.setBounds(324, 668, 127, 180);
-		contentPane.add(lblNewLabel_2_1_1);
+		JLabel lbl21 = new JLabel("");
+		lbl21.setBounds(324, 668, 127, 180);
+		contentPane.add(lbl21);
 		
-		JLabel lblNewLabel_3_1_1 = new JLabel("");
-		lblNewLabel_3_1_1.setBounds(461, 668, 127, 180);
-		contentPane.add(lblNewLabel_3_1_1);
+		JLabel lbl22 = new JLabel("");
+		lbl22.setBounds(461, 668, 127, 180);
+		contentPane.add(lbl22);
 		
-		JLabel lblNewLabel_4_1_1 = new JLabel("");
-		lblNewLabel_4_1_1.setForeground(Color.BLACK);
-		lblNewLabel_4_1_1.setBackground(Color.WHITE);
-		lblNewLabel_4_1_1.setBounds(598, 668, 127, 180);
-		contentPane.add(lblNewLabel_4_1_1);
+		JLabel lbl23 = new JLabel("");
+		lbl23.setForeground(Color.BLACK);
+		lbl23.setBackground(Color.WHITE);
+		lbl23.setBounds(598, 668, 127, 180);
+		contentPane.add(lbl23);
 		
-		JLabel lblNewLabel_5_1_1 = new JLabel("");
-		lblNewLabel_5_1_1.setForeground(Color.BLACK);
-		lblNewLabel_5_1_1.setBackground(Color.WHITE);
-		lblNewLabel_5_1_1.setBounds(735, 668, 127, 180);
-		contentPane.add(lblNewLabel_5_1_1);
+		JLabel lbl24 = new JLabel("");
+		lbl24.setForeground(Color.BLACK);
+		lbl24.setBackground(Color.WHITE);
+		lbl24.setBounds(735, 668, 127, 180);
+		contentPane.add(lbl24);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(1162, 385, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(1162, 419, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
 }
