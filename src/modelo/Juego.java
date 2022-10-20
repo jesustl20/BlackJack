fcc01ada3b199bc00d2fd3ab1d3a4a1eccfe7e10
Alberto <albertolopezgarciaee@gmail.com>
2022-10-20 20:03:00 +0200

    Cositas

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -1,48 +1,52 @@
 package modelo;
 
+import java.awt.event.WindowStateListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.nio.file.Files.*;
 
 public class Juego {
     public static Baraja baraja = new Baraja();
-    private Jugador jugador = new Jugador();
-    private Jugador dealer = new Jugador();
+    public Jugador jugador = new Jugador();
+    public Jugador dealer = new Jugador();
 
     public void iniciarJuego() {
         rellenarBaraja();
         repartirCartas();
     }
 
-    public void rellenarBaraja() {
+    private void rellenarBaraja() {
         try {
-            Path path = Path.of(getClass().getResource("/resources").toURI()); // URL de la carpeta resources EJEMPLO XEXU/ESCRITORIO/PROYECTO/RESOURCES/
-            walk(path, 2) // Recorremos lo que hay en esa carpeta EJEMPLO (file.png, caca.png, carpetaX)
-                    .filter(p -> new File(String.valueOf(p)).isFile()) // Solo dejamos pasar los directorios que sean archivos (NO QUEREMOS CARPETAS) miramos si tiene un punto en la URL porque solo los archivos tienen puntos
-                    .forEach(file -> { // En este foreach vamos a rellenar la baraja de cartas con sus directorios y valores para pintarlas mas facilmente
-                        String fileString = file.toString(); // Para no repetir codigo
-                        int cardValue = Integer.parseInt(fileString.replaceAll("[^0-9]", ""));
+            Path recursos = Path.of(getClass().getResource("/resources").toURI()); // URL de la carpeta resources EJEMPLO XEXU/ESCRITORIO/PROYECTO/RESOURCES/
 
-                        if (cardValue > 10) { // Si es la sota el caballo o el rey vale 10 igualmente
-                            cardValue = 10;
-                        }
+            List<Path> paths = walk(recursos, 2).collect(Collectors.toList()); // De Stream a Lista
 
-                        baraja.cartas.add( // Vamos a rellenar la baraja
-                                new Carta(
-                                        fileString, // Le ponemos como primer atributo el path a la imagen (xexu/escritorio/proyect/corazon9.png)
-                                        cardValue //como el path tiene el número de la carta nos quedamos con el número
-                                )
-                        );
-                    });
+            for (Path path : paths) {
+                File archivo = new File(String.valueOf(path));
+                if (archivo.isFile()) {
+                    String fileString = path.toString();
+
+                    int cardValue = Integer.parseInt(fileString.replaceAll("[^0-9]", ""));
+
+                    if (cardValue > 10) { // Si es la sota el caballo o el rey vale 10 igualmente
+                        cardValue = 10;
+                    }
+
+                    baraja.cartas.add(new Carta(fileString, cardValue));
+                }
+            }
+
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void repartirCartas() {
+    private void repartirCartas() {
         cartaParaJugador();
         cartaParaDealer();
         cartaParaJugador();
@@ -84,19 +88,18 @@ public class Juego {
         }
     }
 
-    public String turnoDealer() {
+    public Carta turnoDealer() {
         int totalPuntos = getTotalPuntos(dealer);
 
         if (totalPuntos < 17) {
-            cartaParaDealer();
-            return "Sigo";
+            return cartaParaDealer();
         } else {
-            return "Planto";
+            return null;
         }
     }
 
     public void turnoDealerSiJugadorSePlanta() {
-        while (getTotalPuntos(dealer) < 17){
+        while (getTotalPuntos(dealer) < 17) {
             cartaParaDealer();
         }
     }
--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -9,13 +9,19 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JButton;
 
 public class Vista extends JFrame {
 
 	public final JButton pedir;
 	public final JButton plantarse;
-	public final JLabel lbl1;
+	public final JButton start;
+
+	public List<JLabel> jlabelsDealer = new ArrayList();
+	public List<JLabel> jlabelsJugador = new ArrayList();
+
 	public JPanel contentPane;
 
 	/**
@@ -48,132 +54,163 @@ public class Vista extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		lbl1 = new JLabel("");
+		JLabel lbl1 = new JLabel("");
 		lbl1.setForeground(Color.BLACK);
 		lbl1.setBackground(Color.WHITE);
 		lbl1.setBounds(51, 54, 127, 180);
 		contentPane.add(lbl1);
-		
+
 		JLabel lbl2 = new JLabel("");
 		lbl2.setBounds(187, 54, 127, 180);
 		contentPane.add(lbl2);
-		
+
 		JLabel lbl3 = new JLabel("");
 		lbl3.setBounds(324, 54, 127, 180);
 		contentPane.add(lbl3);
-		
+
 		JLabel lbl4 = new JLabel("");
 		lbl4.setBounds(461, 54, 127, 180);
 		contentPane.add(lbl4);
-		
+
 		JLabel lbl5 = new JLabel("");
 		lbl5.setForeground(Color.BLACK);
 		lbl5.setBackground(Color.WHITE);
 		lbl5.setBounds(598, 54, 127, 180);
 		contentPane.add(lbl5);
-		
+
 		JLabel lbl6 = new JLabel("");
 		lbl6.setForeground(Color.BLACK);
 		lbl6.setBackground(Color.WHITE);
 		lbl6.setBounds(735, 54, 127, 180);
 		contentPane.add(lbl6);
-		
+
 		JLabel lbl7 = new JLabel("");
 		lbl7.setForeground(Color.BLACK);
 		lbl7.setBackground(Color.WHITE);
 		lbl7.setBounds(51, 245, 127, 180);
 		contentPane.add(lbl7);
-		
+
 		JLabel lbl8 = new JLabel("");
 		lbl8.setBounds(187, 245, 127, 180);
 		contentPane.add(lbl8);
-		
+
 		JLabel lbl9 = new JLabel("");
 		lbl9.setBounds(324, 245, 127, 180);
 		contentPane.add(lbl9);
-		
+
 		JLabel lbl10 = new JLabel("");
 		lbl10.setBounds(461, 245, 127, 180);
 		contentPane.add(lbl10);
-		
+
 		JLabel lbl11 = new JLabel("");
 		lbl11.setForeground(Color.BLACK);
 		lbl11.setBackground(Color.WHITE);
 		lbl11.setBounds(598, 245, 127, 180);
 		contentPane.add(lbl11);
-		
+
 		JLabel lbl12 = new JLabel("");
 		lbl12.setForeground(Color.BLACK);
 		lbl12.setBackground(Color.WHITE);
 		lbl12.setBounds(735, 245, 127, 180);
 		contentPane.add(lbl12);
-		
+
+		jlabelsDealer.add(lbl1);
+		jlabelsDealer.add(lbl2);
+		jlabelsDealer.add(lbl3);
+		jlabelsDealer.add(lbl4);
+		jlabelsDealer.add(lbl5);
+		jlabelsDealer.add(lbl6);
+		jlabelsDealer.add(lbl7);
+		jlabelsDealer.add(lbl8);
+		jlabelsDealer.add(lbl8);
+		jlabelsDealer.add(lbl9);
+		jlabelsDealer.add(lbl10);
+		jlabelsDealer.add(lbl11);
+		jlabelsDealer.add(lbl12);
+
 		JLabel lbl13 = new JLabel("");
 		lbl13.setForeground(Color.BLACK);
 		lbl13.setBackground(Color.WHITE);
 		lbl13.setBounds(51, 477, 127, 180);
 		contentPane.add(lbl13);
-		
+
 		JLabel lbl14 = new JLabel("");
 		lbl14.setBounds(187, 477, 127, 180);
 		contentPane.add(lbl14);
-		
+
 		JLabel lbl15 = new JLabel("");
 		lbl15.setBounds(324, 477, 127, 180);
 		contentPane.add(lbl15);
-		
+
 		JLabel lbl16 = new JLabel("");
 		lbl16.setBounds(461, 477, 127, 180);
 		contentPane.add(lbl16);
-		
+
 		JLabel lbl17 = new JLabel("");
 		lbl17.setForeground(Color.BLACK);
 		lbl17.setBackground(Color.WHITE);
 		lbl17.setBounds(598, 477, 127, 180);
 		contentPane.add(lbl17);
-		
+
 		JLabel lbl18 = new JLabel("");
 		lbl18.setForeground(Color.BLACK);
 		lbl18.setBackground(Color.WHITE);
 		lbl18.setBounds(735, 477, 127, 180);
 		contentPane.add(lbl18);
-		
+
 		JLabel lbl19 = new JLabel("");
 		lbl19.setForeground(Color.BLACK);
 		lbl19.setBackground(Color.WHITE);
 		lbl19.setBounds(51, 668, 127, 180);
 		contentPane.add(lbl19);
-		
+
 		JLabel lbl20 = new JLabel("");
 		lbl20.setBounds(187, 668, 127, 180);
 		contentPane.add(lbl20);
-		
+
 		JLabel lbl21 = new JLabel("");
 		lbl21.setBounds(324, 668, 127, 180);
 		contentPane.add(lbl21);
-		
+
 		JLabel lbl22 = new JLabel("");
 		lbl22.setBounds(461, 668, 127, 180);
 		contentPane.add(lbl22);
-		
+
 		JLabel lbl23 = new JLabel("");
 		lbl23.setForeground(Color.BLACK);
 		lbl23.setBackground(Color.WHITE);
 		lbl23.setBounds(598, 668, 127, 180);
 		contentPane.add(lbl23);
-		
+
 		JLabel lbl24 = new JLabel("");
 		lbl24.setForeground(Color.BLACK);
 		lbl24.setBackground(Color.WHITE);
 		lbl24.setBounds(735, 668, 127, 180);
 		contentPane.add(lbl24);
 
-		pedir = new JButton("New button");
+		jlabelsJugador.add(lbl13);
+		jlabelsJugador.add(lbl14);
+		jlabelsJugador.add(lbl15);
+		jlabelsJugador.add(lbl16);
+		jlabelsJugador.add(lbl17);
+		jlabelsJugador.add(lbl18);
+		jlabelsJugador.add(lbl19);
+		jlabelsJugador.add(lbl20);
+		jlabelsJugador.add(lbl21);
+		jlabelsJugador.add(lbl22);
+		jlabelsJugador.add(lbl23);
+		jlabelsJugador.add(lbl24);
+
+		pedir = new JButton("PEDIR");
 		pedir.setBounds(1162, 385, 89, 23);
 		contentPane.add(pedir);
 
-		plantarse = new JButton("New button");
+		plantarse = new JButton("PLANTARSE");
 		plantarse.setBounds(1162, 419, 89, 23);
 		contentPane.add(plantarse);
+
+		start = new JButton("START");
+		start.setBounds(1162, 553, 89, 23);
+		contentPane.add(start);
 	}
 }
